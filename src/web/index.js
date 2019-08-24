a657d75d8886aa9ac0cc7bb23fc2d51d61019787
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-08-25 00:35:21 +0100

    tf.data.webcam

--- src/web/index.js
+++ src/web/index.js
@@ -1,21 +1,90 @@
+import * as tf from '@tensorflow/tfjs'
 import { solve, getInitialValues } from '../logic'
-import { HARD_PUZZLE } from '../logic/sample-puzzles'
+import * as SP from '../logic/sample-puzzles'
 import { drawInitialGrid, drawSolution } from './svg'
 
-const onOpenCVLoaded = () => {
+const hideSplashContent = () => {
   const splashContentElement = document.querySelector('.splash-content')
-  const mainContentElement = document.querySelector('.main-content')
   splashContentElement.style.display = 'none'
+}
+
+const showMainContent = () => {
+  const mainContentElement = document.querySelector('.main-content')
   mainContentElement.style.display = 'block'
-  const puzzle = HARD_PUZZLE
+}
+
+const solveSamplePuzzle = () => {
+  const puzzle = SP.HARD_PUZZLE
   const initialValues = getInitialValues(puzzle)
   drawInitialGrid(initialValues)
   const solutions = solve(puzzle)
   drawSolution(solutions[0])
 }
 
-const headElement = document.querySelector('head')
-const scriptElement = document.createElement('script')
-scriptElement.setAttribute('src', '/opencv_4.1.1.js')
-scriptElement.onload = onOpenCVLoaded
-headElement.appendChild(scriptElement)
+const videoElement = document.getElementById('video')
+const canvasElement = document.getElementById('canvas')
+const sudokuElement = document.getElementById('sudoku')
+let webcam = undefined
+
+const DISPLAY_MODE_VIDEO = Symbol('DISPLAY_MODE_VIDEO')
+const DISPLAY_MODE_CANVAS = Symbol('DISPLAY_MODE_CANVAS')
+const DISPLAY_MODE_SUDOKU = Symbol('DISPLAY_MODE_SUDOKU')
+
+const setDisplayMode = displayMode => {
+  videoElement.style.display = displayMode === DISPLAY_MODE_VIDEO ? 'block' : 'none'
+  canvasElement.style.display = displayMode === DISPLAY_MODE_CANVAS ? 'block' : 'none'
+  sudokuElement.style.display = displayMode === DISPLAY_MODE_SUDOKU ? 'block' : 'none'
+}
+
+const onClickVideo = async () => {
+  if (webcam) {
+    const imageTensor = await webcam.capture()
+    webcam.stop()
+    webcam = undefined // eslint-disable-line
+    setDisplayMode(DISPLAY_MODE_CANVAS)
+    tf.browser.toPixels(imageTensor, canvasElement)
+  } else {
+    setDisplayMode(DISPLAY_MODE_VIDEO)
+    const videoRect = videoElement.getBoundingClientRect()
+    const webcamConfig = {
+      facingMode: 'environment',
+      resizeWidth: videoRect.width,
+      resizeHeight: videoRect.height
+    }
+    videoElement.width = videoRect.width
+    videoElement.height = videoRect.height
+    webcam = await tf.data.webcam(videoElement, webcamConfig) //eslint-disable-line
+  }
+}
+
+const onClickCanvas = () => {
+  setDisplayMode(DISPLAY_MODE_SUDOKU)
+  solveSamplePuzzle()
+}
+
+const onClickSudoku = () => {
+  setDisplayMode(DISPLAY_MODE_VIDEO)
+}
+
+videoElement.addEventListener('click', onClickVideo)
+canvasElement.addEventListener('click', onClickCanvas)
+sudokuElement.addEventListener('click', onClickSudoku)
+
+const onOpenCVLoaded = () => {
+  hideSplashContent()
+  showMainContent()
+}
+
+const loadOpenCV = () => {
+  const headElement = document.querySelector('head')
+  const scriptElement = document.createElement('script')
+  scriptElement.setAttribute('src', '/opencv_4.1.1.js')
+  scriptElement.onload = onOpenCVLoaded
+  headElement.appendChild(scriptElement)
+}
+
+const main = () => {
+  loadOpenCV()
+}
+
+main()
