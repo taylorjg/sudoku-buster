ed65a89b5fa0c8ff2b20400f3d0357aedcb7b7e4
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-09-14 17:09:17 +0100

    Add more in-browser tests

--- test/index.js
+++ test/index.js
@@ -33,35 +33,81 @@ describe('sudoku-buster tests', () => {
     cellsModel.dispose()
   })
 
-  it('findBoundingBox', async () => {
-    const gridImageTensorNormalised = I.normaliseGridImage(imageData)
-    const [x, y, w, h] = await findBoundingBox(gridImageTensorNormalised)
-    gridImageTensorNormalised.dispose()
-    expect(x).to.be.almost(21, 3)
-    expect(y).to.be.almost(30, 3)
-    expect(w).to.be.almost(184, 3)
-    expect(h).to.be.almost(184, 3)
+  describe('findBoundingBox', () => {
+    it('should find the correct bounding box', async () => {
+      const gridImageTensorNormalised = I.normaliseGridImage(imageData)
+      const [x, y, w, h] = await findBoundingBox(gridImageTensorNormalised)
+      gridImageTensorNormalised.dispose()
+      expect(x).to.be.almost(21, 3)
+      expect(y).to.be.almost(30, 3)
+      expect(w).to.be.almost(184, 3)
+      expect(h).to.be.almost(184, 3)
+    })
   })
 
-  it('scanPuzzle', async () => {
-    const numTensorsBefore = tf.memory().numTensors
-    const digitPredictions = await scanPuzzle(cellsModel, imageData)
-    expect(satisfiesAllConstraints(digitPredictions)).to.be.true
-    const actual = digitPredictionsToPuzzle(digitPredictions)
-    const expected = [
-      "28  3  45",
-      "5 4   6 2",
-      " 1 5 4 9 ",
-      "  28 34  ",
-      "8   7   3",
-      "  36 29  ",
-      " 4 1 5 2 ",
-      "1 5   7 4",
-      "63  4  19"
-    ]
-    expect(actual).to.deep.equal(expected)
-    const numTensorsAfter = tf.memory().numTensors
-    expect(numTensorsAfter).to.equal(numTensorsBefore)
+  describe('scanPuzzle', () => {
+
+    it('should predict the correct initial values', async () => {
+      const digitPredictions = await scanPuzzle(cellsModel, imageData)
+      const actual = digitPredictionsToPuzzle(digitPredictions)
+      const expected = [
+        "28  3  45",
+        "5 4   6 2",
+        " 1 5 4 9 ",
+        "  28 34  ",
+        "8   7   3",
+        "  36 29  ",
+        " 4 1 5 2 ",
+        "1 5   7 4",
+        "63  4  19"
+      ]
+      expect(actual).to.deep.equal(expected)
+    })
+
+    it('should not leak any tensors', async () => {
+      const numTensorsBefore = tf.memory().numTensors
+      await scanPuzzle(cellsModel, imageData)
+      const numTensorsAfter = tf.memory().numTensors
+      expect(numTensorsAfter).to.equal(numTensorsBefore)
+    })
+  })
+
+  describe('satisfiesAllConstraints', () => {
+
+    it('should succeed given an empty collection of predictions', () => {
+      expect(satisfiesAllConstraints([])).to.be.true
+    })
+
+    it('should succeed given the result of a successful scan', async () => {
+      const digitPredictions = await scanPuzzle(cellsModel, imageData)
+      expect(satisfiesAllConstraints(digitPredictions)).to.be.true
+    })
+
+    it('should fail given a row with a duplicate digit', () => {
+      const digitPredictions = [
+        { digitPrediction: 4, index: 0 },
+        { digitPrediction: 9, index: 2 },
+        { digitPrediction: 4, index: 6 }
+      ]
+      expect(satisfiesAllConstraints(digitPredictions)).to.be.false
+    })
+
+    it('should fail given a column with a duplicate digit', () => {
+      const digitPredictions = [
+        { digitPrediction: 7, index: 1 },
+        { digitPrediction: 7, index: 10 },
+        { digitPrediction: 5, index: 19 }
+      ]
+      expect(satisfiesAllConstraints(digitPredictions)).to.be.false
+    })
+
+    it('should fail given a 3x3 box with a duplicate digit', () => {
+      const digitPredictions = [
+        { digitPrediction: 3, index: 2 },
+        { digitPrediction: 3, index: 20 }
+      ]
+      expect(satisfiesAllConstraints(digitPredictions)).to.be.false
+    })
   })
 })
 
