df1e0a056651dc0ae09f1a75a5dae0e0342df8a6
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-09-14 09:14:15 +0100

    Redirect from http to https on Heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const sslRedirect = require('heroku-ssl-redirect')
 const path = require('path')
 const PORT = process.env.PORT || 3090
 const distFolder = path.resolve(__dirname, '..', 'dist')
 const app = express()
+app.use(sslRedirect(['production'], 301))
 app.use(express.static(distFolder))
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
