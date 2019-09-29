c64b2fc563d03aca8cea549add9c09f0837b7986
Administrator <jonathan.taylor.programmer@gmail.com>
2019-09-29 23:07:15 +0100

    Add ability to sort columns

--- analytics/index.js
+++ analytics/index.js
@@ -1,8 +1,7 @@
 import * as R from 'ramda'
 import moment from 'moment'
 import Chart from 'chart.js'
-import { drawInitialValues, drawSolution } from '../src/drawSvg'
-import { createSvgElement } from '../src/drawSvg'
+import { createSvgElement, drawInitialValues, drawSolution } from '../src/drawSvg'
 import { showErrorPanel, hideErrorPanel } from '../src/errorPanel'
 import * as db from './db'
 
@@ -253,13 +252,82 @@ const createDetailsRow = (item, summaryRow) => {
   return detailsRow
 }
 
-const populateTable = data =>
-  data.forEach(createSummaryRow)
+const SORT_COLUMN_VERSION = Symbol('SORT_COLUMN_VERSION')
+const SORT_COLUMN_TIMESTAMP = Symbol('SORT_COLUMN_TIMESTAMP')
+const SORT_COLUMN_OUTCOME = Symbol('SORT_COLUMN_OUTCOME')
+const SORT_COLUMN_DURATION = Symbol('SORT_COLUMN_DURATION')
+const SORT_COLUMN_FRAME_COUNT = Symbol('SORT_COLUMN_FRAME_COUNT')
+const SORT_COLUMN_FPS = Symbol('SORT_COLUMN_FPS')
+
+const SORT_ASCENDING = Symbol('SORT_ASCENDING')
+const SORT_DESCENDING = Symbol('SORT_DESCENDING')
+
+const SORT_COLUMN_FN_MAP = new Map([
+  [SORT_COLUMN_VERSION, item => item.version],
+  [SORT_COLUMN_TIMESTAMP, item => item.timestamp],
+  [SORT_COLUMN_OUTCOME, item => item.outcome],
+  [SORT_COLUMN_DURATION, item => item.duration],
+  [SORT_COLUMN_FRAME_COUNT, item => item.frameCount],
+  [SORT_COLUMN_FPS, item => item.frameCount / item.duration]
+])
+
+const SORT_DIRECTION_FN_MAP = new Map([
+  [SORT_ASCENDING, R.ascend],
+  [SORT_DESCENDING, R.descend]
+])
+
+const SORT_OPPOSITE_DIRECTION_MAP = new Map([
+  [SORT_ASCENDING, SORT_DESCENDING],
+  [SORT_DESCENDING, SORT_ASCENDING]
+])
+
+let data = []
+let currentSortColumn = SORT_COLUMN_TIMESTAMP
+let currentSortDirections = new Map([
+  [SORT_COLUMN_VERSION, SORT_DESCENDING],
+  [SORT_COLUMN_TIMESTAMP, SORT_DESCENDING],
+  [SORT_COLUMN_OUTCOME, SORT_DESCENDING],
+  [SORT_COLUMN_DURATION, SORT_ASCENDING],
+  [SORT_COLUMN_FRAME_COUNT, SORT_ASCENDING],
+  [SORT_COLUMN_FPS, SORT_DESCENDING]
+])
+
+const sort = data => {
+  const columnFn = SORT_COLUMN_FN_MAP.get(currentSortColumn)
+  const currentSortDirection = currentSortDirections.get(currentSortColumn)
+  const directionFn = SORT_DIRECTION_FN_MAP.get(currentSortDirection)
+  return R.sort(directionFn(columnFn), data)
+}
+
+const onColumnClick = column => () => {
+  if (currentSortColumn === column) {
+    const currentSortDirection = currentSortDirections.get(currentSortColumn)
+    const oppositeDirection = SORT_OPPOSITE_DIRECTION_MAP.get(currentSortDirection)
+    currentSortDirections.set(column, oppositeDirection)
+  } else {
+    currentSortColumn = column
+  }
+  data = sort(data)
+  clearTable()
+  data.forEach(item => {
+    tbodyElement.appendChild(item.summaryRow)
+    if (item.summaryRow.detailsRow) {
+      tbodyElement.appendChild(item.summaryRow.detailsRow)
+    }
+  })
+}
+
+const populateTable = data => {
+  data.forEach(item => {
+    item.summaryRow = createSummaryRow(item)
+  })
+}
 
 const refreshTable = () =>
   doDatabaseCall(async () => {
     clearTable()
-    const data = await db.getAll()
+    data = []
+    data = sort(await db.getAll())
     populateTable(data)
     populateLegend(data)
   })
@@ -272,12 +340,57 @@ const refreshButton = document.getElementById('refresh-btn')
 const deleteAllButton = document.getElementById('delete-all-btn')
 const tbodyElement = document.querySelector('table tbody')
 
+const theadElement = document.querySelector('table thead')
+const versionColumnElement = theadElement.querySelector('th:nth-child(1)')
+const timestampColumnElement = theadElement.querySelector('th:nth-child(2)')
+const outcomeColumnElement = theadElement.querySelector('th:nth-child(3)')
+const durationColumnElement = theadElement.querySelector('th:nth-child(4)')
+const frameCountColumnElement = theadElement.querySelector('th:nth-child(5)')
+const fpsColumnElement = theadElement.querySelector('th:nth-child(6)')
+
+versionColumnElement.addEventListener('click', onColumnClick(SORT_COLUMN_VERSION))
+timestampColumnElement.addEventListener('click', onColumnClick(SORT_COLUMN_TIMESTAMP))
+outcomeColumnElement.addEventListener('click', onColumnClick(SORT_COLUMN_OUTCOME))
+durationColumnElement.addEventListener('click', onColumnClick(SORT_COLUMN_DURATION))
+frameCountColumnElement.addEventListener('click', onColumnClick(SORT_COLUMN_FRAME_COUNT))
+fpsColumnElement.addEventListener('click', onColumnClick(SORT_COLUMN_FPS))
+
+const initColumn = columnElement => {
+  columnElement.placeholder = columnElement.querySelector('.placeholder')
+  columnElement.upArrow = columnElement.querySelector('.up-arrow')
+  columnElement.downArrow = columnElement.querySelector('.down-arrow')
+}
+
+initColumn(versionColumnElement)
+initColumn(timestampColumnElement)
+initColumn(outcomeColumnElement)
+initColumn(durationColumnElement)
+initColumn(frameCountColumnElement)
+initColumn(fpsColumnElement)
+
+const updateSortDirection = (column, columnElement) => {
+  const isCurrentSortColumn = column === currentSortColumn
+  const currentSortDirection = currentSortDirections.get(column)
+  const showUpArrow = isCurrentSortColumn && currentSortDirection === SORT_ASCENDING
+  const showDownArrow = isCurrentSortColumn && currentSortDirection === SORT_DESCENDING
+  const showPlaceholder = !showUpArrow && !showDownArrow
+  columnElement.placeholder.style.display = showPlaceholder ? 'inline-block' : 'none'
+  columnElement.upArrow.style.display = showUpArrow ? 'inline-block' : 'none'
+  columnElement.downArrow.style.display = showDownArrow ? 'inline-block' : 'none'
+}
+
 const onIdle = () => {
   loadingSpinnerElement.style.display = databaseCallInProgress ? 'inline-block' : 'none'
   refreshButton.disabled = databaseCallInProgress
   deleteAllButton.disabled = databaseCallInProgress || tbodyElement.childElementCount === 0
   const deleteButtons = tbodyElement.querySelectorAll('tr td button')
   deleteButtons.forEach(deleteButton => deleteButton.disabled = databaseCallInProgress)
+  updateSortDirection(SORT_COLUMN_VERSION, versionColumnElement)
+  updateSortDirection(SORT_COLUMN_TIMESTAMP, timestampColumnElement)
+  updateSortDirection(SORT_COLUMN_OUTCOME, outcomeColumnElement)
+  updateSortDirection(SORT_COLUMN_DURATION, durationColumnElement)
+  updateSortDirection(SORT_COLUMN_FRAME_COUNT, frameCountColumnElement)
+  updateSortDirection(SORT_COLUMN_FPS, fpsColumnElement)
   requestAnimationFrame(onIdle)
 }
 
