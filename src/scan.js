f20236bd2882211ec34f36856cd94863121abb74
Administrator <jonathan.taylor.programmer@gmail.com>
2019-09-30 23:47:15 +0100

    Record performance marks in finally blocks

--- src/scan.js
+++ src/scan.js
@@ -8,73 +8,82 @@ import * as SVG from './drawSvg'
 import { findBoundingBox } from './findBoundingBox'
 
 const findAndCheckBoundingBox = async (imageData, svgElement) => {
-  const result = await findBoundingBox(imageData)
-  svgElement && SVG.clearDiagnostics(svgElement)
-  if (!result) {
-    const error = new Error('Failed to find bounding box.')
-    error.isScanException = true
-    throw error
+  try {
+    const result = await findBoundingBox(imageData)
+    svgElement && SVG.clearDiagnostics(svgElement)
+    if (!result) {
+      const error = new Error('Failed to find bounding box.')
+      error.isScanException = true
+      throw error
+    }
+    const { boundingBox } = result
+    log.info(`[findAndCheckBoundingBox] boundingBox: ${JSON.stringify(boundingBox)}`)
+    const [, , w, h] = boundingBox
+    if (w < C.GRID_IMAGE_WIDTH / 2 || h < C.GRID_IMAGE_HEIGHT / 2) {
+      const error = new Error(`Bounding box is too small, ${JSON.stringify(boundingBox)}.`)
+      error.isScanException = true
+      throw error
+    }
+    return result
+  } finally {
+    performance.mark('findAndCheckBoundingBox')
   }
-  const { boundingBox } = result
-  log.info(`[findAndCheckBoundingBox] boundingBox: ${JSON.stringify(boundingBox)}`)
-  const [, , w, h] = boundingBox
-  if (w < C.GRID_IMAGE_WIDTH / 2 || h < C.GRID_IMAGE_HEIGHT / 2) {
-    const error = new Error(`Bounding box is too small, ${JSON.stringify(boundingBox)}.`)
-    error.isScanException = true
-    throw error
-  }
-  return result
 }
 
 const handleDrawingOptions = (boundingBoxInfo, svgElement, drawingOptions) => {
-  const { contour, corners, boundingBox } = boundingBoxInfo
-  if (drawingOptions.drawContour) {
-    const points = R.splitEvery(2, contour.data32S).map(([x, y]) => ({ x, y }))
-    SVG.drawContour(svgElement, points, 'red')
-  }
-  if (drawingOptions.drawCorners) {
-    SVG.drawCorners(svgElement, corners, 'magenta')
-  }
-  if (drawingOptions.drawBoundingBox) {
-    SVG.drawBoundingBox(svgElement, boundingBox, 'blue')
-  }
-  if (drawingOptions.drawGridSquares) {
-    const gridSquares = CALC.calculateGridSquares(boundingBox)
-    SVG.drawGridSquares(svgElement, gridSquares, 'green')
+  try {
+    const { contour, corners, boundingBox } = boundingBoxInfo
+    if (drawingOptions.drawContour) {
+      const points = R.splitEvery(2, contour.data32S).map(([x, y]) => ({ x, y }))
+      SVG.drawContour(svgElement, points, 'red')
+    }
+    if (drawingOptions.drawCorners) {
+      SVG.drawCorners(svgElement, corners, 'magenta')
+    }
+    if (drawingOptions.drawBoundingBox) {
+      SVG.drawBoundingBox(svgElement, boundingBox, 'blue')
+    }
+    if (drawingOptions.drawGridSquares) {
+      const gridSquares = CALC.calculateGridSquares(boundingBox)
+      SVG.drawGridSquares(svgElement, gridSquares, 'green')
+    }
+  } finally {
+    performance.mark('handleDrawingOptions')
   }
 }
 
 const predictDigits = async (disposables, cellsModel, gridImageTensor, boundingBox) => {
-  const gridSquareImageTensors = D.cropGridSquares(
-    gridImageTensor,
-    boundingBox)
-  disposables.push(gridSquareImageTensors)
-  const batchSize = 81
-  const outputs = cellsModel.predict(gridSquareImageTensors, { batchSize })
-  disposables.push(outputs)
-  const outputsArgMax = outputs.argMax(1)
-  disposables.push(outputsArgMax)
-  const outputsArgMaxArray = await outputsArgMax.array()
-  const indexedDigitPredictions = outputsArgMaxArray
-    .map((digitPrediction, index) => ({ digitPrediction, index }))
-    .filter(({ digitPrediction }) => digitPrediction > 0)
-  return indexedDigitPredictions
+  try {
+    const gridSquareImageTensors = D.cropGridSquares(
+      gridImageTensor,
+      boundingBox)
+    disposables.push(gridSquareImageTensors)
+    const batchSize = 81
+    const outputs = cellsModel.predict(gridSquareImageTensors, { batchSize })
+    disposables.push(outputs)
+    const outputsArgMax = outputs.argMax(1)
+    disposables.push(outputsArgMax)
+    const outputsArgMaxArray = await outputsArgMax.array()
+    const indexedDigitPredictions = outputsArgMaxArray
+      .map((digitPrediction, index) => ({ digitPrediction, index }))
+      .filter(({ digitPrediction }) => digitPrediction > 0)
+    return indexedDigitPredictions
+  } finally {
+    performance.mark('predictDigits')
+  }
 }
 
 export const scanPuzzle = async (cellsModel, imageData, svgElement, drawingOptions = {}) => {
   const disposables = []
   try {
     const boundingBoxInfo = await findAndCheckBoundingBox(imageData, svgElement)
-    performance.mark('findAndCheckBoundingBox')
     handleDrawingOptions(boundingBoxInfo, svgElement, drawingOptions)
-    performance.mark('handleDrawingOptions')
     const { imageDataCorrected } = boundingBoxInfo
     const imageTensorCorrected = I.imageDataToImageTensor(imageDataCorrected)
     disposables.push(imageTensorCorrected)
     performance.mark('imageDataToImageTensor')
     const boundingBox = CALC.inset(0, 0, imageDataCorrected.width, imageDataCorrected.height, 2, 2)
     const indexedDigitPredictions = await predictDigits(disposables, cellsModel, imageTensorCorrected, boundingBox)
-    performance.mark('predictDigits')
     return indexedDigitPredictions
   } finally {
     disposables.forEach(disposable => disposable.dispose())
